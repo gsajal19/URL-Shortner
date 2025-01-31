8fa730059ac87e7630c7dfc23e78a53ded62a826
Sajal Gupta <70812171+gsajal19@users.noreply.github.com>
2025-01-31 23:09:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+const cors = require('cors');
+const http = require('http');
+require('dotenv').config();  
+const connection = mysql.createConnection({
+host:process.env.DB_HOST,
+user: process.env.DB_USER,
+password: process.env.DB_PASSWORD,
+database: process.env.DB_NAME
+});
+const port=5001;
+connection.connect((err)=>{
+if(!err){
+    console.log("Connection Established");
+}else{
+    // console.log("Not Connection Established");
+
+}
+});
+app.use(express.json());
+app.use(cors());
+app.get("/",(req,resp)=>{
+    resp.send("Hey Sajal, App Working ....")
+})
+app.post('/generate',(req,resp)=>{
+    connection.query("INSERT INTO urls(shortid,originalurl,expiration) value('"+ req.body.shortid+" ','"+req.body.originalurl+"', NOW() + INTERVAL "+ req.body.interval +" "+ req.body.intervaltype+")",(err,result)=>{
+        if(err){
+            resp.status(400).send(err);
+        }else{
+    
+            resp.send({
+                status:true,
+                shorturl:`http://${req.headers.host}/${req.body.shortid}`,
+                message:"Validity "+ req.body.interval+" "+req.body.intervaltype
+            });
+        }
+    });
+});
+
+app.get("/:shortid",(req,resp)=>{
+    const shortid = req.params.shortid;
+    connection.query("SELECT * FROM urls WHERE shortid = ? ",[shortid],(err,result)=>{
+        if(err){
+            resp.send({
+                status:false,
+                message:"Something Went Wrong !! Please try again"
+            });
+        }else{
+            if (result.length > 0) {
+                // If a result is found, return it
+                // resp.status(200).send({
+                //     status: true,
+                //     message: "Record found.",
+                //     data: result
+                // });
+                // console.log
+                resp.redirect(301,result[0].originalurl);
+
+            } else {
+                // If no record found with the given shortid
+                resp.status(404).send({
+                    status: false,
+                    message: "URL not found."
+                });
+        }
+     }
+    });
+});
+
+app.listen(port);
